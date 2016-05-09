0e652512555efedcf64f89a670dd1c4bf28d1c65
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2016-05-09 14:55:18 -0400

    Refactor LCM code.

--- src/main/datasources/lcm/LCMVerticle.kt
+++ src/main/datasources/lcm/LCMVerticle.kt
@@ -11,42 +11,65 @@ import org.slf4j.event.Level
 
 class LCMVerticle(var dataURI: String) : AbstractVerticle(), LCMSubscriber {
 
-    //todo: validation of URI
-
     override fun messageReceived(lcmInstance: LCM, channel: String, dis: LCMDataInputStream) {
-        this.log(Level.DEBUG) { "LCM receiving on thread ${Thread.currentThread().name}" }
+        this.log { "LCM receiving on thread ${Thread.currentThread().name}" }
     }
 
     override fun start() {
-        var lcm: LCM
-        var (uriType, uriAddress, topics) = dataURI.split(delimiters = "#",
-                                                          limit = 3,
-                                                          ignoreCase = true)
+
+        var (uriType, uriAddress, topics) = splitURI()
 
         when (uriType.toUpperCase()) {
             "LCMSOCKET" -> {
-                lcm = LCM(uriAddress)
-                log { "LCM listening on $uriAddress" }
-                topics.split(",").forEach {
-                    lcm.subscribe(it) { lcm, name, data ->
-                        log { "Received on $name" }
-                        var navSoln = navigationsolution(data)
-                        log {
-                            asYaml("LCMVerticle_NavSoln",
-                                   "lat" to navSoln.latitude,
-                                   "lon" to navSoln.longitude,
-                                   "alt" to navSoln.altitude)
-                        }
-                    }
-                }
+                listenLCMSocket(topics, uriAddress)
+            }
+            "LCMFILE"   -> {
+                throw NotImplementedError()
             }
         }
 
-        this.log(Level.DEBUG) { "LCM verticle running on thread ${Thread.currentThread().name}" }
+        log { "LCM verticle running on thread ${Thread.currentThread().name}" }
     }
 
     override fun stop() {
         var lcm = LCM.getSingleton()
         lcm.close()
     }
+
+    /**
+     * Listens to the socket defined by the LCM address [uriAddress] on the topics in
+     * [topics].
+     */
+    private fun listenLCMSocket(topics: List<String>, uriAddress: String): LCM {
+        var lcm = LCM(uriAddress)
+        log { "LCM listening on $uriAddress" }
+        topics.forEach {
+            lcm.subscribe(it) { lcm, name, data ->
+                log { "Received on $name" }
+                var navSoln = navigationsolution(data)
+                log {
+                    asYaml("LCMVerticle_NavSoln",
+                           "lat" to navSoln.latitude,
+                           "lon" to navSoln.longitude,
+                           "alt" to navSoln.altitude)
+                }
+            }
+        }
+        return lcm
+    }
+
+    private fun splitURI(): Triple<String, String, List<String>> {
+        try {
+            var (uriType, uriAddress, topics) = dataURI.split(delimiters = "#",
+                                    limit = 3,
+                                    ignoreCase = true)
+            return Triple(uriType, uriAddress, topics.split(","))
+
+        } catch(e: IndexOutOfBoundsException) {
+            log(Level.ERROR) { "Incorrectly formatted LCM URI received: '$dataURI'" }
+            vertx.close()
+            throw(IllegalArgumentException("Incorrectly formatted LCM URI received: '$dataURI'"))
+        }
+    }
+
 }
