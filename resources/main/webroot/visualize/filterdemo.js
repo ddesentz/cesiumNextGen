77ef25ae7c5bd99f7446ed8ba11bf148df3ca17b
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2016-05-26 16:37:40 -0400

    Formatting pass.

--- resources/main/webroot/visualize/filterdemo.js
+++ resources/main/webroot/visualize/filterdemo.js
@@ -23,21 +23,21 @@ function startEventBus(url) {
     var eb = new EventBus(url);
     eb.onopen = function () {
         console.log("Requesting server send list of topics.");
-        eb.send("cesium_topics", "req", function(err, msgs) {
+        eb.send("cesium_topics", "req", function (err, msgs) {
             console.log("Got topic list from server.");
-            msgs.body.forEach(function(topic) {
+            msgs.body.forEach(function (topic) {
                 console.log("Connecting to " + topic);
                 count++;
                 vehicles.push(createPlaneAt(INIT_POS, INIT_ROT));
                 //If more than 4 planes are created generate random colors for their paths
-                if(count > 4){
+                if (count > 4) {
                     colorArray.push(randomColor());
                 }
-                aircraftPaths.push(createPath(colorArray[count-1]));
+                aircraftPaths.push(createPath(colorArray[count - 1]));
                 pathCoords.push([]);
-                vehicles[vehicles.length-1].position = INIT_POS;
-                connectPlaneToBus(eb, vehicles[vehicles.length-1], topic);
-                viewer.trackedEntity = vehicles[vehicles.length-1];
+                vehicles[vehicles.length - 1].position = INIT_POS;
+                connectPlaneToBus(eb, vehicles[vehicles.length - 1], topic);
+                viewer.trackedEntity = vehicles[vehicles.length - 1];
             })
         });
     };
@@ -50,17 +50,17 @@ function connectPlaneToBus(eb, plane, msgname) {
             // Invert lon/lat to lat/lon
             var newPos = new Cesium.Cartographic(message.body.pos[1], message.body.pos[0], message.body.pos[2]);
             var newRot;
-            if (!message.body.rot){
+            if (!message.body.rot) {
                 plane.model.uri = undefined;
                 plane.box = {
-                                dimensions : new Cesium.Cartesian3(5.0, 5.0, 5.0),
-                                material : colorArray[count-1]
-                            };
+                    dimensions: new Cesium.Cartesian3(5.0, 5.0, 5.0),
+                    material: colorArray[count - 1]
+                };
             }
             else
                 newRot = message.body.rot;
-                updatePosition(newPos, plane);
-                updateOrientation(newRot, plane);
+            updatePosition(newPos, plane);
+            updateOrientation(newRot, plane);
 
         } catch (e) {
             console.error("Received a pose from server that didnt have pos/vel/rot correctly formatted.");
@@ -72,7 +72,7 @@ function connectPlaneToBus(eb, plane, msgname) {
 function createPlaneAt(pos, rot) {
 
     return viewer.entities.add({
-        id : count,
+        id: count,
         position: INIT_POS,
         orientation: INIT_ROT,
         model: {
@@ -85,36 +85,36 @@ function createPlaneAt(pos, rot) {
 
 function updatePosition(posLLH, vehicle) {
     vehicle.position = Cesium.Cartesian3.fromRadians(posLLH.longitude, posLLH.latitude, posLLH.height);
-    aircraftPaths[vehicle.id-1].polyline.positions = Cesium.Cartesian3.fromRadiansArrayHeights(updatePath(posLLH.longitude, posLLH.latitude, posLLH.height,vehicle.id-1));
+    aircraftPaths[vehicle.id - 1].polyline.positions = Cesium.Cartesian3.fromRadiansArrayHeights(updatePath(posLLH.longitude, posLLH.latitude, posLLH.height, vehicle.id - 1));
 }
 
 function updateOrientation(rotMat, vehicle) {
     vehicle.orientation = new Cesium.ConstantProperty(Cesium.Transforms.headingPitchRollQuaternion(INIT_POS, rotMat[2], rotMat[1], rotMat[0]));
 }
 
-function updatePath(long,lat,height,id){
+function updatePath(long, lat, height, id) {
     var path = pathCoords[id];
     path.push(long);
     path.push(lat);
     path.push(height);
     //Set Max path length for 10mins @ 2Hz
-    if(path.length == (MAX_PATH_LEN)){
-        var removed = path.splice(0,3);
+    if (path.length == (MAX_PATH_LEN)) {
+        var removed = path.splice(0, 3);
     }
     return path;
 }
 
-function createPath(color){
+function createPath(color) {
     return viewer.entities.add({
-    name : "Plane " + count + " Path",
-        polyline : {
-            positions : [],
-            width : 5,
-            material : color
+        name: "Plane " + count + " Path",
+        polyline: {
+            positions: [],
+            width: 5,
+            material: color
         }
     });
 }
 
-function randomColor(){
-    return new Cesium.Color(Math.random(),Math.random(),Math.random(),1);
+function randomColor() {
+    return new Cesium.Color(Math.random(), Math.random(), Math.random(), 1);
 }
